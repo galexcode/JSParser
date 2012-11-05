e9badc23b7276cb8528a361c4399db58aed11ef3
Rachel Brindle <you@subluminal.net>
2012-11-05 00:32:50 -0500

    Initial Commit

---
+++ JSParser/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  JSParser
+//
+//  Created by Rachel Brindle on 11/5/12.
+//  Copyright (c) 2012 Rachel Brindle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
